e67b43d0dff4e6723bd424e0172320095def9a4c
Michal Zmuda <zmu.michal@gmail.com>
2016-05-08 13:18:38 +0200

    design download

--- configurator/urls.py
+++ configurator/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'/run', views.run, name='run'),
+    url(r'/get_design', views.get_design, name='get_design'),
     url(r'/generate', views.generate, name='generate'),
     url(r'^', views.index, name='index'),
 ]
--- configurator/views.py
+++ configurator/views.py
@@ -3,6 +3,7 @@ import subprocess
 
 from django.http import HttpResponse
 from django.template import loader
+from django.utils.encoding import smart_str
 from configurator.logic import generation
 
 
@@ -13,18 +14,23 @@ def index(request):
 
 
 def generate(request):
-    _do_generate(request)
-    return HttpResponse()
-
-
-def run(request):
-    _do_generate(request)
-    res = subprocess.Popen("cd out && chmod a+x up.sh && ./up.sh", shell=True, stdout=subprocess.PIPE).stdout.read()
-    print res
-    return HttpResponse()
-
-
-def _do_generate(request):
     edges = json.loads(request.POST['edges'])
     nodes = json.loads(request.POST['nodes'])
     generation.run(nodes, edges, loader, 'out')
+    return HttpResponse('{"result":"ok"}')
+
+
+def get_design(request):
+    content = open('out/design.json').read()
+    response = HttpResponse(content, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str('design.json')
+    return response
+
+
+def run(request):
+    edges = json.loads(request.POST['edges'])
+    nodes = json.loads(request.POST['nodes'])
+    generation.run(nodes, edges, loader, 'out')
+    res = subprocess.Popen("cd out && chmod a+x up.sh && ./up.sh", shell=True, stdout=subprocess.PIPE).stdout.read()
+    print res  # todo: handle
+    return HttpResponse('{"result":"ok"}')
