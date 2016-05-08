7aea36751dce7834af3afb23f6c44ac41a5d54b6
Michal Zmuda <zmu.michal@gmail.com>
2016-05-08 11:50:44 +0200

    running sibling containers

--- configurator/urls.py
+++ configurator/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'/run', views.run, name='run'),
     url(r'/generate', views.generate, name='generate'),
     url(r'^', views.index, name='index'),
 ]
--- configurator/views.py
+++ configurator/views.py
@@ -1,8 +1,8 @@
 import json
+import subprocess
 
 from django.http import HttpResponse
 from django.template import loader
-
 from configurator.logic import generation
 
 
@@ -13,9 +13,18 @@ def index(request):
 
 
 def generate(request):
+    _do_generate(request)
+    return HttpResponse()
+
+
+def run(request):
+    _do_generate(request)
+    res = subprocess.Popen("cd out && chmod a+x up.sh && ./up.sh", shell=True, stdout=subprocess.PIPE).stdout.read()
+    print res
+    return HttpResponse()
+
+
+def _do_generate(request):
     edges = json.loads(request.POST['edges'])
     nodes = json.loads(request.POST['nodes'])
-    print edges
-    print nodes
     generation.run(nodes, edges, loader, 'out')
-    return HttpResponse()
